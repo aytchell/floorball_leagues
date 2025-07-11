a318cd8bfaf90aeb819f15e3a8ffb38ff31269d5
Hannes Lerchl <hannes.lerchl@googlemail.com>
2025-07-12 00:25:48 +0200

    We can fetch and parse the initial json document

--- lib/main.dart
+++ lib/main.dart
@@ -1,122 +1,214 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'JSON Fetcher',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: JsonFetcherPage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class JsonFetcherPage extends StatefulWidget {
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _JsonFetcherPageState createState() => _JsonFetcherPageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _JsonFetcherPageState extends State<JsonFetcherPage> {
+  String _seasonsText = '';
+  bool _isLoading = false;
+  String _errorMessage = '';
 
-  void _incrementCounter() {
+
+  Future<void> _fetchSeasons() async {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _isLoading = true;
+      _errorMessage = '';
+      _seasonsText = '';
     });
+
+    try {
+      final response = await http.get(
+        Uri.parse('https://www.saisonmanager.de/api/v2/init.json'),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      );
+
+      if (response.statusCode == 200) {
+        final jsonData = json.decode(response.body);
+        final apiResponse = ApiResponse.fromJson(jsonData);
+        
+        setState(() {
+          // _seasonsText = apiResponse.seasons.map((season) => season.name as String).toList().join('\n');
+           _seasonsText = apiResponse.gameOperations.map((gameOp) => gameOp.name as String).toList().join('\n');
+          _isLoading = false;
+        });
+      } else {
+        setState(() {
+          _errorMessage = 'Failed to load data. Status: ${response.statusCode}';
+          _isLoading = false;
+        });
+      }
+    } catch (e) {
+      setState(() {
+        _errorMessage = 'Error: ${e.toString()}';
+        _isLoading = false;
+      });
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text('JSON Fetcher'),
+        backgroundColor: Colors.blue,
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            ElevatedButton(
+              onPressed: _isLoading ? null : _fetchSeasons,
+              child: _isLoading
+                  ? Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        SizedBox(
+                          width: 20,
+                          height: 20,
+                          child: CircularProgressIndicator(
+                            strokeWidth: 2,
+                            valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+                          ),
+                        ),
+                        SizedBox(width: 10),
+                        Text('Loading...'),
+                      ],
+                    )
+                  : Text('Fetch Seasons'),
+            ),
+            SizedBox(height: 20),
+            if (_errorMessage.isNotEmpty)
+              Container(
+                padding: EdgeInsets.all(12),
+                decoration: BoxDecoration(
+                  color: Colors.red.shade100,
+                  border: Border.all(color: Colors.red),
+                  borderRadius: BorderRadius.circular(4),
+                ),
+                child: Text(
+                  _errorMessage,
+                  style: TextStyle(color: Colors.red.shade700),
+                ),
+              ),
+            if (_errorMessage.isNotEmpty) SizedBox(height: 20),
+            Expanded(
+              child: Container(
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.grey),
+                  borderRadius: BorderRadius.circular(4),
+                ),
+                child: TextField(
+                  controller: TextEditingController(text: _seasonsText),
+                  maxLines: null,
+                  expands: true,
+                  readOnly: true,
+                  decoration: InputDecoration(
+                    hintText: 'Seasons will appear here...',
+                    border: InputBorder.none,
+                    contentPadding: EdgeInsets.all(12),
+                  ),
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontFamily: 'monospace',
+                  ),
+                ),
+              ),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+// Data model for the API response
+class ApiSeasonInfo {
+  int? id;
+  String? name;
+  bool current = false;
+
+  ApiSeasonInfo({this.id, this.name, this.current = false});
+
+  factory ApiSeasonInfo.fromJson(Map<String, dynamic> json) {
+    return ApiSeasonInfo(
+      id: json['id'] as int?,
+      name: json['name'] as String?,
+      current: json['current'] as bool? ?? false,
+    );
+  }
+}
+
+class GameOperation {
+  int? id;
+  String? name;
+  String? shortName;
+  String? path;
+  String? logoUrl;
+  String? logoQuadUrl;
+
+  GameOperation({
+    this.id,
+    this.name,
+    this.shortName,
+    this.path,
+    this.logoUrl,
+    this.logoQuadUrl,
+  });
+
+  factory GameOperation.fromJson(Map<String, dynamic> json) {
+    return GameOperation(
+      id: json['id'] as int?,
+      name: json['name'] as String?,
+      shortName: json['short_name'] as String?,
+      path: json['path'] as String?,
+      logoUrl: json['logo_url'] as String?,
+      logoQuadUrl: json['logo_quad_url'] as String?,
+    );
+  }
+}
+
+class ApiResponse {
+  final List<ApiSeasonInfo> seasons;
+  final int? currentSeasonId;
+  final List<GameOperation> gameOperations;
+
+  ApiResponse({required this.seasons, this.currentSeasonId, required this.gameOperations});
+
+  factory ApiResponse.fromJson(Map<String, dynamic> json) {
+    var seasonsJson = json['seasons'] as List? ?? [];
+    var operationsJson = json['game_operations'] as List? ?? [];
+    return ApiResponse(
+      seasons: seasonsJson
+          .map((seasonJson) => ApiSeasonInfo.fromJson(seasonJson))
+          .toList(),
+      currentSeasonId: json['current_season_id'] as int?,
+      gameOperations: operationsJson
+          .map((operationJson) => GameOperation.fromJson(operationJson))
+          .toList(),
     );
   }
 }
