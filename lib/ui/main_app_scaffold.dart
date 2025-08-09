adfb0bc2e06cdc83c8b4a31e7b4a1156562b2ea4
Hannes Lerchl <hannes.lerchl@googlemail.com>
2025-08-09 14:59:39 +0200

    Added menu bar (yet without functionality)

--- lib/ui/main_app_scaffold.dart
+++ lib/ui/main_app_scaffold.dart
@@ -7,6 +7,7 @@ class MainAppScaffold extends StatelessWidget {
   final Widget? drawer;
   final List<Widget>? actions;
   final bool showBackButton;
+  final bool showBottomNavigation;
 
   const MainAppScaffold({
     super.key,
@@ -15,6 +16,7 @@ class MainAppScaffold extends StatelessWidget {
     this.drawer,
     this.actions,
     this.showBackButton = false,
+    this.showBottomNavigation = true,
   });
 
   @override
@@ -29,6 +31,60 @@ class MainAppScaffold extends StatelessWidget {
       ),
       drawer: drawer,
       body: body,
+      bottomNavigationBar: showBottomNavigation ? _buildBottomNavigationBar() : null,
+    );
+  }
+
+  Widget _buildBottomNavigationBar() {
+    return Container(
+      height: 60,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.3),
+            spreadRadius: 1,
+            blurRadius: 5,
+            offset: Offset(0, -2),
+          ),
+        ],
+      ),
+      child: Row(
+        children: [
+          _buildBottomNavItem(
+            icon: Icons.home,
+            onTap: () {
+              // TODO: Add home navigation functionality
+            },
+          ),
+          _buildBottomNavItem(
+            icon: Icons.date_range,
+            onTap: () {
+              // TODO: Add date range functionality
+            },
+          ),
+          Spacer(), // Pushes icons to the left
+        ],
+      ),
+    );
+  }
+
+  Widget _buildBottomNavItem({
+    required IconData icon,
+    required VoidCallback onTap,
+  }) {
+    return InkWell(
+      onTap: onTap,
+      borderRadius: BorderRadius.circular(8),
+      child: Container(
+        width: 60,
+        height: 60,
+        child: Icon(
+          icon,
+          color: Colors.blue[600],
+          size: 24,
+        ),
+      ),
     );
   }
 }
