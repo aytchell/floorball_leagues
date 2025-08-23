990bc2af15f2c52217e7e3a5693c07126756de13
Hannes Lerchl <hannes.lerchl@googlemail.com>
2025-08-23 10:02:53 +0200

    Added penalties

--- lib/ui/team_details/team_scorer_table.dart
+++ lib/ui/team_details/team_scorer_table.dart
@@ -1,3 +1,5 @@
+import 'dart:math' as math;
+
 import 'package:flutter/material.dart';
 import 'package:material_table_view/material_table_view.dart';
 import '../../api/models/scorer.dart';
@@ -9,15 +11,115 @@ class IndexedScorer {
   IndexedScorer({required this.index, required this.scorer});
 }
 
-class TeamScorerTable extends StatelessWidget {
-  final List<IndexedScorer> scorers;
+enum SortColumn {
+  rank,
+  name,
+  games,
+  points,
+  goals,
+  assists,
+  penalty2,
+  penalty2and2,
+  penalty10,
+  penaltyMsTech,
+  penaltyMsFull,
+}
 
-  TeamScorerTable({required this.scorers});
+class TeamScorerTable extends StatefulWidget {
+  final List<IndexedScorer> scorers;
+  final maxRank;
+
+  TeamScorerTable({required this.scorers})
+    : maxRank = (scorers.isEmpty
+          ? 0
+          : scorers.map((idx) => idx.index).reduce(math.max));
+
+  @override
+  _TeamScorerTableState createState() => _TeamScorerTableState();
+}
+
+class _TeamScorerTableState extends State<TeamScorerTable> {
+  List<IndexedScorer> _sortedScorers = [];
+  SortColumn? _currentSortColumn;
+  bool _isAscending = true;
+
+  @override
+  void initState() {
+    super.initState();
+    _sortedScorers = List.from(widget.scorers);
+  }
+
+  @override
+  void didUpdateWidget(TeamScorerTable oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    if (widget.scorers != oldWidget.scorers) {
+      _sortedScorers = List.from(widget.scorers);
+      _applySorting();
+    }
+  }
+
+  void _sortBy(SortColumn column) {
+    setState(() {
+      if (_currentSortColumn == column) {
+        _isAscending = !_isAscending;
+      } else {
+        _currentSortColumn = column;
+        _isAscending = true;
+      }
+      _applySorting();
+    });
+  }
+
+  void _applySorting() {
+    if (_currentSortColumn == null) return;
+
+    _sortedScorers.sort((a, b) {
+      int comparison = 0;
+
+      switch (_currentSortColumn!) {
+        case SortColumn.rank:
+          comparison = a.index.compareTo(b.index);
+          break;
+        case SortColumn.name:
+          comparison = a.scorer.fullName.compareTo(b.scorer.fullName);
+          break;
+        case SortColumn.games:
+          comparison = a.scorer.games.compareTo(b.scorer.games);
+          break;
+        case SortColumn.points:
+          comparison = a.scorer.points.compareTo(b.scorer.points);
+          break;
+        case SortColumn.goals:
+          comparison = a.scorer.goals.compareTo(b.scorer.goals);
+          break;
+        case SortColumn.assists:
+          comparison = a.scorer.assists.compareTo(b.scorer.assists);
+          break;
+        case SortColumn.penalty2:
+          comparison = a.scorer.penalty2.compareTo(b.scorer.penalty2);
+          break;
+        case SortColumn.penalty2and2:
+          comparison = a.scorer.penalty2and2.compareTo(b.scorer.penalty2and2);
+          break;
+        case SortColumn.penalty10:
+          comparison = a.scorer.penalty10.compareTo(b.scorer.penalty10);
+          break;
+        case SortColumn.penaltyMsTech:
+          comparison = a.scorer.penaltyMsTech.compareTo(b.scorer.penaltyMsTech);
+          break;
+        case SortColumn.penaltyMsFull:
+          comparison = a.scorer.penaltyMsFull.compareTo(b.scorer.penaltyMsFull);
+          break;
+      }
+
+      return _isAscending ? comparison : -comparison;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     // Handle empty scorers list
-    if (scorers.isEmpty) {
+    if (widget.scorers.isEmpty) {
       return Container(
         padding: const EdgeInsets.all(16),
         decoration: BoxDecoration(
@@ -31,7 +133,7 @@ class TeamScorerTable extends StatelessWidget {
       );
     }
 
-    final rankColumnWidth = (scorers.last.index >= 100) ? 30.0 : 20.0;
+    final rankColumnWidth = (widget.maxRank >= 100) ? 30.0 : 20.0;
 
     return Container(
       height: 300,
@@ -50,10 +152,15 @@ class TeamScorerTable extends StatelessWidget {
           const TableColumn(width: 40.0), // points column
           const TableColumn(width: 40.0), // goals column
           const TableColumn(width: 40.0), // assists column
+          const TableColumn(width: 40.0), // penalty: 2'
+          const TableColumn(width: 40.0), // penalty: 2+2
+          const TableColumn(width: 40.0), // penalty: 10'
+          const TableColumn(width: 40.0), // technical match penalty
+          const TableColumn(width: 40.0), // match penalty
         ],
-        rowCount: scorers.length,
+        rowCount: _sortedScorers.length,
         rowHeight: 34.0,
-        headerHeight: 60.0, // Increased height for rotated text
+        headerHeight: 75.0, // Increased height for rotated text
         headerBuilder: (context, contentBuilder) {
           return Container(
             decoration: BoxDecoration(
@@ -65,21 +172,55 @@ class TeamScorerTable extends StatelessWidget {
             child: contentBuilder(context, (context, column) {
               switch (column) {
                 case 0:
-                  return _rotatedTableHeader('Platz');
+                  return _buildSortableHeader('Platz', SortColumn.rank, true);
                 case 1:
-                  return Container(
-                    alignment: Alignment.center,
-                    padding: const EdgeInsets.symmetric(horizontal: 8),
-                    child: _tableHeaderText('Name'),
-                  );
+                  return _buildSortableHeader('Name', SortColumn.name, false);
                 case 2:
-                  return _rotatedTableHeader('Spiele');
+                  return _buildSortableHeader('Spiele', SortColumn.games, true);
                 case 3:
-                  return _rotatedTableHeader('Punkte');
+                  return _buildSortableHeader(
+                    'Punkte',
+                    SortColumn.points,
+                    true,
+                  );
                 case 4:
-                  return _rotatedTableHeader('Tore');
+                  return _buildSortableHeader('Tore', SortColumn.goals, true);
                 case 5:
-                  return _rotatedTableHeader('Assists');
+                  return _buildSortableHeader(
+                    'Assists',
+                    SortColumn.assists,
+                    true,
+                  );
+                case 6:
+                  return _buildSortableHeader(
+                    "Strafe 2'",
+                    SortColumn.penalty2,
+                    true,
+                  );
+                case 7:
+                  return _buildSortableHeader(
+                    'Strafe 2+2',
+                    SortColumn.penalty2and2,
+                    true,
+                  );
+                case 8:
+                  return _buildSortableHeader(
+                    "Strafe 10'",
+                    SortColumn.penalty10,
+                    true,
+                  );
+                case 9:
+                  return _buildSortableHeader(
+                    'MS (tech)',
+                    SortColumn.penaltyMsTech,
+                    true,
+                  );
+                case 10:
+                  return _buildSortableHeader(
+                    'MS',
+                    SortColumn.penaltyMsFull,
+                    true,
+                  );
                 default:
                   return const SizedBox.shrink();
               }
@@ -87,8 +228,8 @@ class TeamScorerTable extends StatelessWidget {
           );
         },
         rowBuilder: (context, row, contentBuilder) {
-          final rank = scorers[row].index;
-          final scorer = scorers[row].scorer;
+          final rank = _sortedScorers[row].index;
+          final scorer = _sortedScorers[row].scorer;
 
           return Material(
             type: MaterialType.transparency,
@@ -124,6 +265,16 @@ class TeamScorerTable extends StatelessWidget {
                     return _scorerData('${scorer.goals}');
                   case 5:
                     return _scorerData('${scorer.assists}');
+                  case 6:
+                    return _scorerData('${scorer.penalty2}');
+                  case 7:
+                    return _scorerData('${scorer.penalty2and2}');
+                  case 8:
+                    return _scorerData('${scorer.penalty10}');
+                  case 9:
+                    return _scorerData('${scorer.penaltyMsTech}');
+                  case 10:
+                    return _scorerData('${scorer.penaltyMsFull}');
                   default:
                     return const SizedBox.shrink();
                 }
@@ -135,20 +286,56 @@ class TeamScorerTable extends StatelessWidget {
     );
   }
 
-  Widget _rotatedTableHeader(String text) {
-    return Container(
-      alignment: Alignment.center,
-      child: RotatedBox(quarterTurns: 3, child: _tableHeaderText(text)),
+  Widget _buildSortableHeader(String text, SortColumn column, bool rotated) {
+    final isCurrentSort = _currentSortColumn == column;
+    final sortIcon = isCurrentSort
+        ? (_isAscending ? Icons.arrow_upward : Icons.arrow_downward)
+        : Icons.unfold_more;
+
+    final iconColor = isCurrentSort ? Colors.blue[700] : Colors.grey[600];
+    final textColor = isCurrentSort ? Colors.blue[800] : Colors.grey[800];
+
+    Widget content = InkWell(
+      onTap: () => _sortBy(column),
+      child: Container(
+        alignment: Alignment.center,
+        padding: const EdgeInsets.symmetric(horizontal: 4),
+        child: rotated
+            ? RotatedBox(
+                quarterTurns: 3,
+                child: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    _tableHeaderText(text, textColor),
+                    const SizedBox(width: 4),
+                    RotatedBox(
+                      quarterTurns: 1, // Counter-rotate the icon
+                      child: Icon(sortIcon, size: 12, color: iconColor),
+                    ),
+                  ],
+                ),
+              )
+            : Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  _tableHeaderText(text, textColor),
+                  const SizedBox(width: 4),
+                  Icon(sortIcon, size: 12, color: iconColor),
+                ],
+              ),
+      ),
     );
+
+    return content;
   }
 
-  Widget _tableHeaderText(String text) {
+  Widget _tableHeaderText(String text, Color? color) {
     return Text(
       text,
       style: TextStyle(
         fontWeight: FontWeight.bold,
         fontSize: 12,
-        color: Colors.grey[800],
+        color: color ?? Colors.grey[800],
       ),
     );
   }
