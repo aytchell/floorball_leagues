44b5a3fced9f5a3ae36580bd20418928ea99ff90
Hannes Lerchl <hannes.lerchl@googlemail.com>
2026-03-05 23:20:02 +0100

    League names are added for each game history entry

--- lib/ui/views/history/history_page.dart
+++ lib/ui/views/history/history_page.dart
@@ -1,4 +1,6 @@
 import 'package:floorball/blocs/games_visit_history_cubit.dart';
+import 'package:floorball/ui/theme/global_colors.dart';
+import 'package:floorball/ui/widgets/generic_league_name_entry.dart';
 import 'package:flutter/material.dart';
 import 'package:floorball/ui/main_app_scaffold.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -32,14 +34,28 @@ class _GamesVisitHistoryList extends StatelessWidget {
       return Center(child: Text('History Page - Coming Soon'));
     }
 
-    return Column(
-      children: visitedGames
-          .map(
-            (game) => Text(
-              'Game ${game.gameId}: ${game.detailedGame.homeTeamName} vs ${game.detailedGame.guestTeamName}',
-            ),
-          )
-          .toList(),
+    return Container(
+      color: FloorballColors.gray231,
+      padding: EdgeInsetsGeometry.symmetric(vertical: 32.0, horizontal: 8.0),
+      child: Column(
+        children: visitedGames
+            .map((game) => _buildGameEntry(game))
+            .expand((i) => i)
+            .toList(),
+      ),
     );
+  }
+
+  List<Widget> _buildGameEntry(VisitedGame game) {
+    return [
+      GenericLeagueNameEntry(
+        leagueId: game.detailedGame.leagueId,
+        leagueName: game.leagueName,
+        leadingChild: SizedBox(width: 40, height: 40),
+      ),
+      Text(
+        'Game ${game.gameId}: ${game.detailedGame.homeTeamName} vs ${game.detailedGame.guestTeamName}',
+      ),
+    ];
   }
 }
