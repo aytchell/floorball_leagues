e8f1ea214cfe0be3ecc641aa6f3422cda5535dd3
Hannes Lerchl <hannes.lerchl@googlemail.com>
2026-03-24 10:44:48 +0100

    Change version numbers

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -20,14 +20,13 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "de.aytchell.floorball.leagues.app"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
-        versionCode = flutter.versionCode
-        versionName = flutter.versionName
+        versionCode = 260324150
+        versionName = "1.0 beta1"
     }
 
     buildTypes {
