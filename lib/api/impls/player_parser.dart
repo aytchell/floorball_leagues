efee6ad45bafcca716fca0a772593b9775004286
Hannes Lerchl <hannes.lerchl@googlemail.com>
2026-05-01 15:56:28 +0200

    Bugfix (kind of): Add special case for parsing player_name

--- lib/api/impls/player_parser.dart
+++ lib/api/impls/player_parser.dart
@@ -5,7 +5,7 @@ import 'package:floorball/api/impls/string_parser.dart';
 Player parsePlayer(Map<String, dynamic> json) {
   return Player(
     playerId: parseInt(json, 'player_id'),
-    playerName: parseString(json, 'player_name'),
+    playerName: _parsePlayerName(json),
     jerseyNumber: parseInt(json, 'trikot_number'),
     playerFirstname: parseString(json, 'player_firstname'),
     position: parseString(json, 'position'),
@@ -13,6 +13,15 @@ Player parsePlayer(Map<String, dynamic> json) {
     captain: json['captain'] as bool?,
   );
 }
+
+// ok, this is super weird: there is this one single game at
+// https://2324.archiv.saisonmanager.de/ost/1475-u11-junioren-kleinfeld-platzierungsrunde-ost/spiel/35372
+// Where Floor Fighters player nr 80 doesn't have a "player_name" but instead
+// a "plaayer_name". I don't have any idea how this can happen ...
+String _parsePlayerName(Map<String, dynamic> json) =>
+    parseNullableString(json, 'player_name') ??
+    parseNullableString(json, 'plaayer_name') ??
+    "";
 
 Players parsePlayers(Map<String, dynamic> json) {
   var homeJson = json['home'] as List? ?? [];
