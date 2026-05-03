ac340ccaf2acfec1c9beac4499fab90d5f1b66d8
Hannes Lerchl <hannes.lerchl@googlemail.com>
2026-05-03 12:04:54 +0200

    Fix "old-style, erroneous" player names

--- lib/api/impls/player_parser.dart
+++ lib/api/impls/player_parser.dart
@@ -3,15 +3,40 @@ import 'package:floorball/api/impls/int_parser.dart';
 import 'package:floorball/api/impls/string_parser.dart';
 
 Player parsePlayer(Map<String, dynamic> json) {
+  final (playerFirstName, playerName) = _parseCompleteName(json);
   return Player(
     playerId: parseNullableInt(json, 'player_id'),
-    playerName: _parsePlayerName(json),
+    playerName: playerName,
     jerseyNumber: parseNullableInt(json, 'trikot_number'),
-    playerFirstname: parseString(json, 'player_firstname'),
+    playerFirstname: playerFirstName,
     position: parseString(json, 'position'),
     goalkeeper: json['goalkeeper'] as bool?,
     captain: json['captain'] as bool?,
   );
+}
+
+(String, String) _parseCompleteName(Map<String, dynamic> json) {
+  final playerName = _parsePlayerName(json);
+  final playerFirstName = parseNullableString(json, 'player_firstname');
+
+  if (playerFirstName != null) {
+    return (playerFirstName, playerName);
+  }
+
+  // In some very old games (2020 and before) we have players where the
+  // player_firstname is null and the player_name contains both parts, e.g
+  //        "player_firstname": null,
+  //        "player_name": "Mustermann, Max",
+  // Since there are also names with two commas, a simple 'split' is
+  // not sufficient
+  final match = RegExp(r'^(.*?),(.*)$').firstMatch(playerName);
+  if (match != null && match.groupCount == 2) {
+    return (match.group(2)!.trim(), match.group(1)!.trim());
+  }
+
+  // Everything else will be takes as-is (which is better then throwing
+  // an exception ...)
+  return ('', playerName);
 }
 
 // ok, this is super weird: there is this one single game at
